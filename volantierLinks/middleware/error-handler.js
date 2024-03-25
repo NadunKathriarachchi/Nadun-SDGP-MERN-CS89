3ddd71b9561ee10fa1e9c6f92155cad18b5483d4
nadun kathriarachchi <nadunkathriarachchi@gmail.com>
2024-03-25 10:22:02 +0530

    try api binding

--- volantierLinks/middleware/error-handler.js
+++ volantierLinks/middleware/error-handler.js
@@ -1,4 +1,6 @@
 const { constants } = require("../constants");
+// const cors = require("cors"); // Import the CORS middleware
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
@@ -15,28 +17,40 @@ const errorHandler = (err, req, res, next) => {
         message: err.message,
         stackTrace: err.stack,
       });
+     // break; 
     case constants.UNAUTHORIZED:
       res.json({
         title: "Unauthorized",
         message: err.message,
         stackTrace: err.stack,
       });
+     // break;
     case constants.FORBIDDEN:
       res.json({
         title: "Forbidden",
         message: err.message,
         stackTrace: err.stack,
       });
+      //break;
     case constants.SERVER_ERROR:
       res.json({
         title: "Server Error",
         message: err.message,
         stackTrace: err.stack,
       });
+     // break;
     default:
       console.log("No Error, All good !");
       break;
   }
 };
+
+// // Add CORS middleware before your errorHandler middleware
+// const corsOptions = {
+//   origin: 'http://localhost:3000', // Allow requests from localhost:3000
+// };
+// const corsMiddleware = cors(corsOptions);
+
+// module.exports = [corsMiddleware, errorHandler]; // Export both middlewares
 
 module.exports = errorHandler;
